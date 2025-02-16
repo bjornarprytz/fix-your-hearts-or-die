c84021577354a46c59c5e18f4a8d18443dc90353
Bjørnar Prytz <bjornarprytz@gmail.com>
2025-02-17 00:02:49 +0100

    Adjusted floor and camera slightly

--- src/tank_controls.gd
+++ src/tank_controls.gd
@@ -1,27 +1,27 @@
 extends CharacterBody3D
 
 @export var move_amount: float = 2.0
-@export var move_speed: float = 1.0  
-@export var rotation_duration: float = 0.6  
-@export var move_duration: float = 0.4  
+@export var move_speed: float = 1.0
+@export var rotation_duration: float = 0.6
+@export var move_duration: float = 0.4
 @export var button_delay: float = 0.2
 
 
-var is_moving: bool = false  # on button push at a time
+var is_moving: bool = false # on button push at a time
 
 func _input(event: InputEvent) -> void:
 	if is_moving:
-		return  # Ignore input while in action
+		return # Ignore input while in action
 
 	if event.is_action_pressed("ui_left"):
-		rotate_character(90)  
+		rotate_character(90)
 	elif event.is_action_pressed("ui_right"):
-		rotate_character(-90) 
+		rotate_character(-90)
 	elif event.is_action_pressed("ui_up"):
 		move_forward()
 
 func rotate_character(degrees: float) -> void:
-	is_moving = true  # pause other movement inputs
+	is_moving = true # pause other movement inputs
 
 	# rotate
 	var target_rotation = transform.basis.rotated(Vector3.UP, deg_to_rad(degrees))
